ad79f95f1a8439b8016db73b630b28dc0c14a0eb
Leon Li <lioung860302@gmail.com>
2025-02-27 15:21:06 +0800

    test: add get accounts test

--- internal/services/accounts/internal/accounts/features/get_accounts/validates/get_accounts_request.go
+++ internal/services/accounts/internal/accounts/features/get_accounts/validates/get_accounts_request.go
@@ -1,8 +1,6 @@
 package validates
 
 import (
-	"net/http"
-
 	customizeerrors "github.com/Leon180/go-event-driven-microservices/internal/pkg/customize_errors"
 	"github.com/Leon180/go-event-driven-microservices/internal/pkg/enums"
 	featuresdtos "github.com/Leon180/go-event-driven-microservices/internal/services/accounts/internal/accounts/features/get_accounts/dtos"
@@ -10,9 +8,9 @@ import (
 
 func ValidateGetAccountsByMobileNumberRequest(req featuresdtos.GetAccountsByMobileNumberRequest) error {
 	if req.MobileNumber == "" {
-		return customizeerrors.NewError(http.StatusBadRequest, int(customizeerrors.HTTPBadRequest), "Mobile number is required")
+		return customizeerrors.AccountInvalidMobileNumberError
 	} else if !enums.MobileNumberFormat.ValidateFormat(req.MobileNumber) {
-		return customizeerrors.NewError(http.StatusBadRequest, int(customizeerrors.HTTPBadRequest), "Mobile number is invalid")
+		return customizeerrors.AccountInvalidMobileNumberError
 	}
 	return nil
 }
